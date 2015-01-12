9604a989248aba0fad36c32c04f9e8de2225ecfe
lepickle <mark_jethro27@yahoo.com>
2015-01-12 21:52:22 +0800

    report wizard added and improved some controls

--- app/build/generated/source/r/debug/com/example/mark/ireportmaininterface/R.java
+++ app/build/generated/source/r/debug/com/example/mark/ireportmaininterface/R.java
@@ -18,43 +18,52 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f080015;
-        public static final int action_settings=0x7f08001a;
+        public static final int action_settings=0x7f080020;
         public static final int btnBack=0x7f080007;
-        public static final int btnSelectPhoto=0x7f080019;
+        public static final int btnNext=0x7f080011;
+        public static final int btnPrevious=0x7f080012;
+        public static final int btnSelectPhoto=0x7f08001e;
         public static final int btnSubmit=0x7f080006;
-        public static final int captionText=0x7f080017;
+        public static final int buttonlayout=0x7f08001a;
+        public static final int captionText=0x7f08001c;
         public static final int confirmpass=0x7f080004;
         public static final int editText=0x7f08000e;
         public static final int emailadd=0x7f080009;
-        public static final int infoDisplay=0x7f080012;
+        public static final int hlo=0x7f080001;
+        public static final int infoDisplay=0x7f080017;
         public static final int lblError=0x7f080008;
-        public static final int lblPassword=0x7f080011;
-        public static final int lblUserName=0x7f080010;
-        public static final int login=0x7f080013;
+        public static final int lblPassword=0x7f080016;
+        public static final int lblUserName=0x7f080015;
+        public static final int login=0x7f080018;
+        public static final int mainlayout=0x7f080010;
+        public static final int mytextview=0x7f080013;
         public static final int password=0x7f080002;
         public static final int radioButton=0x7f08000c;
         public static final int radioButton2=0x7f08000d;
-        public static final int register=0x7f080014;
-        public static final int selectCategory=0x7f080018;
-        public static final int textView=0x7f080001;
+        public static final int radiolayout=0x7f080014;
+        public static final int register=0x7f080019;
+        public static final int selectCategory=0x7f08001d;
+        public static final int textView1=0x7f08001f;
         public static final int textView2=0x7f080003;
         public static final int textView3=0x7f080005;
         public static final int textView4=0x7f08000a;
         public static final int textView5=0x7f08000b;
         public static final int textView6=0x7f08000f;
         public static final int username=0x7f080000;
-        public static final int viewImage=0x7f080016;
+        public static final int viewImage=0x7f08001b;
     }
     public static final class layout {
         public static final int activity_create_account=0x7f030000;
-        public static final int activity_login_menu=0x7f030001;
-        public static final int activity_report=0x7f030002;
+        public static final int activity_create_report=0x7f030001;
+        public static final int activity_login_menu=0x7f030002;
+        public static final int activity_report=0x7f030003;
+        public static final int prompt=0x7f030004;
     }
     public static final class menu {
         public static final int create_account=0x7f070000;
-        public static final int login_menu=0x7f070001;
-        public static final int report=0x7f070002;
+        public static final int create_report=0x7f070001;
+        public static final int login_menu=0x7f070002;
+        public static final int report=0x7f070003;
     }
     public static final class string {
         public static final int Back=0x7f050000;
@@ -75,8 +84,9 @@ public final class R {
         public static final int select_category=0x7f05000f;
         public static final int submit=0x7f050010;
         public static final int title_activity_create_account=0x7f050011;
-        public static final int title_activity_login_menu=0x7f050012;
-        public static final int username=0x7f050013;
+        public static final int title_activity_create_report=0x7f050012;
+        public static final int title_activity_login_menu=0x7f050013;
+        public static final int username=0x7f050014;
     }
     public static final class style {
         /**  Customize your theme here. 
